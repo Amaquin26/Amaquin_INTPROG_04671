f9b99799a48afbe4cf3e026dc43ecf0f880ef637
unknown <UU630@ccs.local>
2024-02-24 06:28:22 +0800

    Init commit for PhotoAlbum

---
+++ PhotoAlbum_Amaquin/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PhotoAlbum_Amaquin.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
